aaecd332b859c73d06d5bf8b6183f8242a5c2b38
GitHub-Shota <118955117+GitHub-Shota@users.noreply.github.com>
2022-11-24 15:07:20 +0900

    Update level1.cpp

--- level1.cpp
+++ level1.cpp
@@ -1,1 +1,24 @@
 
+#include <opencv2\opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+void mouse_callback(int event, int x, int y, int flags, void* userdata)
+{
+    if (event == EVENT_MOUSEMOVE) {
+        cout << "(" << x << ", " << y << ")" << endl;
+    }
+}
+
+int main()
+{
+    Mat img(Size(1920,1080),CV_8UC3,Scalar(0,0,0) );
+    imshow("example", img);
+    setMouseCallback("example", mouse_callback);
+    waitKey();
+
+    return 0;
+}
