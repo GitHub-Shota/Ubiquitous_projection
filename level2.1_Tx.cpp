71cb4ff976dabbd0f0093ff321e93d37914b7a91
Yuya <84259422+Git-Yuya@users.noreply.github.com>
2022-12-04 13:13:04 +0900

    Update level2.1_Tx.cpp

--- level2.1_Tx.cpp
+++ level2.1_Tx.cpp
@@ -17,8 +17,27 @@
 // inet_addr()関数で警告が出る場合は以下で警告を無効化
 #pragma warning(disable:4996)
 
+// プロトタイプ宣言
+void draw_line(int event, int x, int y, int flags, void* userdata);
+
+// ソケット通信winsockの立ち上げ
+// wsaDataはエラー取得時に使用
+WSAData wsaData;
+
+// MAKEWORD(2, 0)はwinsockのバージョン2.0
+int err = WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+// socket作成
+// socketを通してデータのやり取りを行う
+// socket(アドレスファミリ, ソケットタイプ, プロトコル)
+// AF_INETはIPv4、SOCK_DGRAMはUDP通信、0はプロトコルファミリーの種類ごとに一つのプロトコルのみをサポート
+SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+// アドレス等を格納
+struct sockaddr_in addr;
+
 // バッファのサイズ
-#define BUFF_SIZE 8
+constexpr auto BUFF_SIZE = 8;
 
 // 右クリックの状態（押された状態:true, 離した状態:false）
 bool is_clicked = false;
@@ -29,15 +48,9 @@ int old_x = 0, old_y = 0, new_x = 0, new_y = 0;
 // パソコンサイズの黒画面
 cv::Mat img(cv::Size(1920, 1080), CV_8UC3, cv::Scalar(0, 0, 0));
 
-// 線を描写
-void draw_line(int event, int x, int y, int flags, void* userdata)
-{
-    // ソケット通信winsockの立ち上げ
-    // wsaDataはエラー取得時に使用
-    WSAData wsaData;
 
-    // MAKEWORD(2, 0)はwinsockのバージョン2.0
-    int err = WSAStartup(MAKEWORD(2, 0), &wsaData);
+int main()
+{
     if (err != 0)
     {
         switch (err)
@@ -60,22 +73,17 @@ void draw_line(int event, int x, int y, int flags, void* userdata)
         }
     }
 
-    // socket作成 
-    // socketを通してデータのやり取りを行う
-    // socket(アドレスファミリ, ソケットタイプ, プロトコル)
-    // AF_INETはIPv4、SOCK_DGRAMはUDP通信
-    // 0はプロトコルファミリーの種類ごとに一つのプロトコルのみをサポート
-    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock == INVALID_SOCKET)
     {
-        std::cout << "socket failed" << std::endl;
+        std::cout << "Socket failed." << std::endl;
     }
 
-    // アドレス等を格納
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;     // IPv4
-    addr.sin_port = htons(50008);  // 待ち受けポート番号設定
-    addr.sin_addr.S_un.S_addr = inet_addr("192.168.11.12");  // 送信アドレスを設定
+    // IPv4
+    addr.sin_family = AF_INET;
+    // 待ち受けポート番号設定
+    addr.sin_port = htons(50008);
+    // 送信アドレスを設定
+    addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 
     // タイムアウト時間の設定
     // struct timeval tv;
@@ -85,7 +93,54 @@ void draw_line(int event, int x, int y, int flags, void* userdata)
     // {
     //     perror("Error");
     // }
+    
+    // 表示するウィンドウに名前を付ける
+    cv::namedWindow("Level2.1_Tx", cv::WINDOW_NORMAL);
+    // フルスクリーン表示
+    //cv::setWindowProperty("Level2.1_Tx", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
 
+    // マウスイベントの登録
+    cv::setMouseCallback("Level2.1_Tx", draw_line);
+
+    // qが押されるまで表示を継続
+    while (1)
+    {
+        cv::imshow("Level2.1_Tx", img);
+
+        // キーイベント
+        int key = cv::waitKey(1);
+        // qキーが押されたときwhileループから抜ける(quit)
+        if (key == 113)
+        {
+            char keyy[BUFF_SIZE] = "quit";
+            sendto(sock, keyy, sizeof(keyy), 0, (struct sockaddr*)&addr, sizeof(addr));
+            break;
+        }
+        // rキーで全消し(reset)
+        else if (key == 114)
+        {
+            char keyy[BUFF_SIZE] = "reset";
+            sendto(sock, keyy, sizeof(keyy), 0, (struct sockaddr*)&addr, sizeof(addr));
+            cv::circle(img, cv::Point(new_x, new_y), 1920, cv::Scalar(0, 0, 0), -1, cv::LINE_AA);
+        }
+    }
+
+    // ウィンドウを閉じる
+    cv::destroyAllWindows();
+
+    // socketの破棄
+    closesocket(sock);
+
+    // すべてのスレッドのwinsockを終了
+    WSACleanup();
+
+    return 0;
+}
+
+
+// 座標の送信と線の描写
+void draw_line(int event, int x, int y, int flags, void* userdata)
+{
     // 今の座標と一つ前の座標を更新
     old_x = new_x;
     old_y = new_y;
@@ -104,7 +159,7 @@ void draw_line(int event, int x, int y, int flags, void* userdata)
         is_clicked = true;
     }
 
-    // 右クリックされている状態に座標送信と点を描写
+    // 右クリックされている状態に座標送信と線を描写
     if (is_clicked == true)
     {
         // 数値→文字列
@@ -128,47 +183,4 @@ void draw_line(int event, int x, int y, int flags, void* userdata)
     {
         is_clicked = false;
     }
-
-    // socketの破棄
-    closesocket(sock);
-
-    // すべてのスレッドのwinsockを終了
-    WSACleanup();
-}
-
-
-int main()
-{
-
-    // 表示するウィンドウに名前を付ける
-    cv::namedWindow("Level2.1_Tx", cv::WINDOW_NORMAL);
-    // フルスクリーン表示
-    //cv::setWindowProperty("Level2.1_Tx", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
-
-    // マウスイベントの登録
-    cv::setMouseCallback("Level2.1_Tx", draw_line);
-
-    // qが押されるまで表示を継続
-    while (1)
-    {
-        cv::imshow("Level2.1_Tx", img);
-
-        // キーイベント
-        int key = cv::waitKey(1);
-        // qキーが押されたときwhileループから抜ける(quit)
-        if (key == 113)
-        {
-            break;
-        }
-        // rキーで全消し(reset)
-        else if (key == 114)
-        {
-            cv::circle(img, cv::Point(new_x, new_y), 1920, cv::Scalar(0, 0, 0), -1, cv::LINE_AA);
-        }
-    }
-
-    // ウィンドウを閉じる
-    cv::destroyAllWindows();
-
-    return 0;
 }
