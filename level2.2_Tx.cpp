d18bd7d71f97791340ef3d463c7336232584829d
Yuya <84259422+Git-Yuya@users.noreply.github.com>
2022-12-14 23:57:33 +0900

    Update level2.2_Tx.cpp

--- level2.2_Tx.cpp
+++ level2.2_Tx.cpp
@@ -188,7 +188,9 @@ void draw_line(int event, int x, int y, int flags, void* userdata)
         sprintf(new_yy, "%d", new_y);
 
         std::cout << "old_x " << old_x << std::endl;
+        std::cout << "old_y " << old_y << std::endl;
         std::cout << "new_x " << new_x << std::endl;
+        std::cout << "new_y " << new_y << std::endl;
 
         // データ送信
         // sendto(ソケット, 送信データ, データのバイト数, フラグ, アドレス情報, アドレス情報のサイズ);
